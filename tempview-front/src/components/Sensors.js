062635465da4db5049f17d5c4d8b0547cfb3a8ee
Lauri Vuorenkoski <lauri.vuorenkoski@helsinki.fi>
2022-05-10 08:47:14 +0300

    sensorlist refactored

---
+++ tempview-front/src/components/Sensors.js
@@ -0,0 +1,174 @@
+import { Table, Row, Col, Form, Button } from 'react-bootstrap'
+import { useQuery, useMutation } from '@apollo/client'
+import { useState } from 'react'
+import {
+  ALL_SENSORS,
+  DELETE_SENSOR,
+  UPDATE_SENSOR,
+  NEW_SENSOR,
+} from '../queries'
+
+const Sensors = () => {
+  const [displaySensorForm, setDisplaySensorForm] = useState(false)
+  const [sensorName, setSensorName] = useState('')
+  const [sensorUnit, setSensorUnit] = useState('')
+  const [sensorFullname, setSensorFullname] = useState('')
+  const [sensorId, setSensorId] = useState(-1)
+  const sensors = useQuery(ALL_SENSORS)
+
+  const [deleteSensor] = useMutation(DELETE_SENSOR, {
+    refetchQueries: [{ query: ALL_SENSORS }],
+  })
+
+  const [updateSensor] = useMutation(UPDATE_SENSOR, {
+    refetchQueries: [{ query: ALL_SENSORS }],
+  })
+
+  const [newSensor] = useMutation(NEW_SENSOR, {
+    refetchQueries: [{ query: ALL_SENSORS }],
+  })
+
+  let sensorList = null
+  if (sensors.data) {
+    sensorList = sensors.data.allSensors
+  }
+
+  const handeDeleteSensor = (id) => {
+    const variables = { deleteSensorId: Number(id) }
+    deleteSensor({ variables })
+  }
+
+  const handeUpdateSensor = (id) => {
+    setDisplaySensorForm(true)
+    const sensor = sensorList.filter((s) => s.id === id)[0]
+    setSensorName(sensor.sensorName)
+    setSensorFullname(sensor.sensorFullname)
+    setSensorUnit(sensor.sensorUnit)
+    setSensorId(Number(sensor.id))
+  }
+
+  const handeNewSensor = (id) => {
+    setDisplaySensorForm(true)
+    setSensorName('')
+    setSensorFullname('')
+    setSensorUnit('')
+    setSensorId(-1)
+  }
+
+  const handleSubmitSensor = async (event) => {
+    event.preventDefault()
+    if (sensorId === -1) {
+      const variables = { sensorName, sensorFullname, sensorUnit }
+      newSensor({ variables })
+    } else {
+      const variables = {
+        sensorName,
+        sensorFullname,
+        sensorUnit,
+        updateSensorId: sensorId,
+      }
+      updateSensor({ variables })
+    }
+    setDisplaySensorForm(false)
+  }
+
+  return (
+    <Row className="p-4" style={{ caretColor: 'white' }}>
+      <h2>Sensorit</h2>
+      <Col className="col-8">
+        <Row>
+          <Table striped>
+            <tbody>
+              <tr>
+                <th>Nimi</th>
+                <th>Kuvaus</th>
+                <th>Mittayksikkö</th>
+                <th>Id</th>
+                <th></th>
+                <th></th>
+              </tr>
+              {sensorList.map((a) => (
+                <tr key={a.id}>
+                  <td>{a.sensorName}</td>
+                  <td>{a.sensorFullname}</td>
+                  <td>{a.sensorUnit}</td>
+                  <td>{a.id}</td>
+                  <td>
+                    <button
+                      style={{ all: 'unset', color: 'red', cursor: 'hand' }}
+                      onClick={() => handeDeleteSensor(a.id)}
+                    >
+                      poista
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      style={{ all: 'unset', color: 'blue', cursor: 'hand' }}
+                      onClick={() => handeUpdateSensor(a.id)}
+                    >
+                      päivitä
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Row>
+
+        {displaySensorForm && (
+          <Row className="align-items-end">
+            <Col className="col-2">
+              <Form>
+                <Form.Group>
+                  <Form.Label>Nimi</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={sensorName}
+                    onChange={({ target }) => setSensorName(target.value)}
+                  />
+                </Form.Group>
+              </Form>
+            </Col>
+            <Col className="col-4">
+              <Form>
+                <Form.Group>
+                  <Form.Label>Kuvaus</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={sensorFullname}
+                    onChange={({ target }) => setSensorFullname(target.value)}
+                  />
+                </Form.Group>
+              </Form>
+            </Col>
+            <Col className="col-2">
+              <Form>
+                <Form.Group>
+                  <Form.Label>Mittayksikkö</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={sensorUnit}
+                    onChange={({ target }) => setSensorUnit(target.value)}
+                  />
+                </Form.Group>
+              </Form>
+            </Col>
+            <Col className="col-2">
+              <Button onClick={handleSubmitSensor}>päivitä/lisää</Button>
+            </Col>
+            <Col className="col-2">
+              <Button onClick={() => setDisplaySensorForm(false)}>
+                peruuta
+              </Button>
+            </Col>
+          </Row>
+        )}
+        {!displaySensorForm && (
+          <Button onClick={() => handeNewSensor()}>Lisää uusi</Button>
+        )}
+      </Col>
+    </Row>
+  )
+}
+
+export default Sensors
--- tempview-front/src/components/Settings.js
+++ tempview-front/src/components/Settings.js
@@ -1,41 +1,12 @@
-import { Table, Row, Col, Form, Button } from 'react-bootstrap'
-import { useQuery, useMutation } from '@apollo/client'
-import { useState } from 'react'
-import {
-  ALL_USERS,
-  ALL_SENSORS,
-  SENSOR_TOKEN,
-  DELETE_SENSOR,
-  UPDATE_SENSOR,
-  NEW_SENSOR,
-} from '../queries'
+import { Table, Row, Col } from 'react-bootstrap'
+import { useQuery } from '@apollo/client'
+import Sensors from './Sensors'
+
+import { ALL_USERS, SENSOR_TOKEN } from '../queries'
 
 const Settings = () => {
-  const [displaySensorForm, setDisplaySensorForm] = useState(false)
-  const [sensorName, setSensorName] = useState('')
-  const [sensorUnit, setSensorUnit] = useState('')
-  const [sensorFullname, setSensorFullname] = useState('')
-  const [sensorId, setSensorId] = useState(-1)
   const users = useQuery(ALL_USERS)
-  const sensors = useQuery(ALL_SENSORS)
   const sensorToken = useQuery(SENSOR_TOKEN)
-
-  const [deleteSensor] = useMutation(DELETE_SENSOR, {
-    refetchQueries: [{ query: ALL_SENSORS }],
-  })
-
-  const [updateSensor] = useMutation(UPDATE_SENSOR, {
-    refetchQueries: [{ query: ALL_SENSORS }],
-  })
-
-  const [newSensor] = useMutation(NEW_SENSOR, {
-    refetchQueries: [{ query: ALL_SENSORS }],
-  })
-
-  let sensorList = null
-  if (sensors.data) {
-    sensorList = sensors.data.allSensors
-  }
 
   let userList = null
   if (users.data) {
@@ -45,45 +16,6 @@ const Settings = () => {
   let token = ''
   if (sensorToken.data) {
     token = sensorToken.data.sensorToken.value
-  }
-
-  const handeDeleteSensor = (id) => {
-    const variables = { deleteSensorId: Number(id) }
-    deleteSensor({ variables })
-  }
-
-  const handeUpdateSensor = (id) => {
-    setDisplaySensorForm(true)
-    const sensor = sensorList.filter((s) => s.id === id)[0]
-    setSensorName(sensor.sensorName)
-    setSensorFullname(sensor.sensorFullname)
-    setSensorUnit(sensor.sensorUnit)
-    setSensorId(Number(sensor.id))
-  }
-
-  const handeNewSensor = (id) => {
-    setDisplaySensorForm(true)
-    setSensorName('')
-    setSensorFullname('')
-    setSensorUnit('')
-    setSensorId(-1)
-  }
-
-  const handleSubmitSensor = async (event) => {
-    event.preventDefault()
-    if (sensorId === -1) {
-      const variables = { sensorName, sensorFullname, sensorUnit }
-      newSensor({ variables })
-    } else {
-      const variables = {
-        sensorName,
-        sensorFullname,
-        sensorUnit,
-        updateSensorId: sensorId,
-      }
-      updateSensor({ variables })
-    }
-    setDisplaySensorForm(false)
   }
 
   return (
@@ -113,84 +45,7 @@ const Settings = () => {
         {token}
       </Row>
 
-      <Row className="p-4">
-        <h2>Sensorit</h2>
-        <Col className="col-8">
-          <Table striped>
-            <tbody>
-              <tr>
-                <th>Nimi</th>
-                <th>Kuvaus</th>
-                <th>Mittayksikkö</th>
-                <th>Id</th>
-                <th></th>
-                <th></th>
-              </tr>
-              {sensorList.map((a) => (
-                <tr key={a.id}>
-                  <td>{a.sensorName}</td>
-                  <td>{a.sensorFullname}</td>
-                  <td>{a.sensorUnit}</td>
-                  <td>{a.id}</td>
-                  <td>
-                    <button
-                      style={{ all: 'unset', color: 'red', cursor: 'hand' }}
-                      onClick={() => handeDeleteSensor(a.id)}
-                    >
-                      poista
-                    </button>
-                  </td>
-                  <td>
-                    <button
-                      style={{ all: 'unset', color: 'blue', cursor: 'hand' }}
-                      onClick={() => handeUpdateSensor(a.id)}
-                    >
-                      päivitä
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-          {displaySensorForm && (
-            <Form onSubmit={handleSubmitSensor}>
-              <Form.Group className="mb-3">
-                <Form.Label>Nimi</Form.Label>
-                <Form.Control
-                  type="text"
-                  value={sensorName}
-                  onChange={({ target }) => setSensorName(target.value)}
-                />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Kuvaus</Form.Label>
-                <Form.Control
-                  type="text"
-                  value={sensorFullname}
-                  onChange={({ target }) => setSensorFullname(target.value)}
-                />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Mittayksikkö</Form.Label>
-                <Form.Control
-                  type="text"
-                  value={sensorUnit}
-                  onChange={({ target }) => setSensorUnit(target.value)}
-                />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                päivitä/lisää
-              </Button>
-              <Button onClick={() => setDisplaySensorForm(false)}>
-                peruuta
-              </Button>
-            </Form>
-          )}
-          {!displaySensorForm && (
-            <Button onClick={() => handeNewSensor()}>Lisää uusi</Button>
-          )}
-        </Col>
-      </Row>
+      <Sensors />
     </div>
   )
 }
