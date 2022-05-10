854f42f857573dded857a7d3c411483374f3ab11
Lauri Vuorenkoski <lauri.vuorenkoski@helsinki.fi>
2022-05-10 12:29:18 +0300

    build included

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 npm install --prefix tempview-back/
-npm install --prefix tempview-front/
-npm run build --prefix tempview-front/
+# npm install --prefix tempview-front/
+# npm run build --prefix tempview-front/
 sudo service tempview restart
