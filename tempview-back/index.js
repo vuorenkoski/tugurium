28cbf93d7eba18607c4af55af0c0c605c62551e2
Lauri Vuorenkoski <lauri.vuorenkoski@helsinki.fi>
2022-05-13 13:51:54 +0300

    change to apollo-server-express

--- tempview-back/index.js
+++ tempview-back/index.js
@@ -1,35 +1,19 @@
-const { ApolloServer } = require('apollo-server')
+const { ApolloServer } = require('apollo-server-express')
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
+const express = require('express')
+const http = require('http')
+
 const { Sequelize } = require('sequelize')
 const { connectToDatabase } = require('./util/db')
 const { User } = require('./models')
 
 const jwt = require('jsonwebtoken')
-const { SECRET, SENSOR_TOKEN } = require('./util/config')
+const { SECRET, SENSOR_TOKEN, PORT, DATABASE_URL } = require('./util/config')
 const typeDefs = require('./schema')
 const resolvers = require('./resolvers')
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: async ({ req }) => {
-    let data = {}
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.toLowerCase().startsWith('bearer ')
-    ) {
-      data['token'] = req.headers.authorization.substring(7)
-      if (data['token'] !== SENSOR_TOKEN) {
-        const decodedToken = jwt.verify(data['token'], SECRET)
-        data['currentUser'] = await User.findOne({
-          where: { id: decodedToken.id },
-        })
-      }
-    }
-    return data
-  },
-})
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
@@ -38,7 +22,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 })
 
-const main = async () => {
+const start = async () => {
   try {
     await connectToDatabase()
     await sequelize.authenticate()
@@ -47,9 +31,44 @@ const main = async () => {
   } catch (error) {
     console.error('Unable to connect to the database:', error)
   }
+
+  const app = express()
+
+  app.get('/images', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+  })
+
+  const httpServer = http.createServer(app)
+
+  const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+  const server = new ApolloServer({
+    schema,
+    context: async ({ req }) => {
+      const auth = req ? req.headers.authorization : null
+      if (auth && auth.toLowerCase().startsWith('bearer ')) {
+        let data = {}
+        data['token'] = auth.substring(7)
+        if (data['token'] !== SENSOR_TOKEN) {
+          const decodedToken = jwt.verify(data['token'], SECRET)
+          data['currentUser'] = await User.findByPk(decodedToken.id)
+        }
+        return data
+      }
+    },
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  })
+
+  await server.start()
+
+  server.applyMiddleware({
+    app,
+    path: '/',
+  })
+
+  httpServer.listen(PORT, () =>
+    console.log(`Server is now running on http://localhost:${PORT}`)
+  )
 }
 
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`)
-  main()
-})
+start()
--- tempview-back/util/config.js
+++ tempview-back/util/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   DATABASE_URL: process.env.DATABASE_URL,
-  PORT: process.env.PORT || 3001,
+  PORT: process.env.PORT || 4000,
   SECRET: process.env.SECRET,
   SENSOR_TOKEN: process.env.SENSOR_TOKEN,
   ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
