dc95cab6cf56c060706046c8bcadc4f7880cb518
Lauri Vuorenkoski <lauri.vuorenkoski@helsinki.fi>
2022-06-01 10:49:33 +0300

    folder names changed

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-npm install --prefix tempview-back/
-# npm install --prefix tempview-front/
-# npm run build --prefix tempview-front/
-sudo service tempview restart
+npm install --prefix tugurium-back/
+# npm install --prefix tugurium-front/
+# npm run build --prefix tugurium-front/
+sudo service tugurium restart
--- getFmiData.sh
+++ getFmiData.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/pi/tempview/tempview-back
+cd /home/pi/tugurium/tugurium-back
 /usr/bin/node getFmiData.js
