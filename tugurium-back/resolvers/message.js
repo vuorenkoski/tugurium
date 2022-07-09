7c6f7512df8be2db1e04b07704ed8785300c4ea0
Lauri Vuorenkoski <lauri.vuorenkoski@helsinki.fi>
2022-07-09 15:18:55 +0300

    send message as email

--- tugurium-back/resolvers/message.js
+++ tugurium-back/resolvers/message.js
@@ -1,7 +1,16 @@
+const nodemailer = require('nodemailer')
 const { AuthenticationError } = require('apollo-server')
 const { Message } = require('../models')
 const { PubSub } = require('graphql-subscriptions')
 const pubsub = new PubSub()
+const {
+  EMAIL_HOST,
+  EMAIL_PASSWORD,
+  EMAIL_PORT,
+  EMAIL_TO,
+  EMAIL_USERNAME,
+  EMAIL_FROM,
+} = require('../util/config')
 
 const allMessages = async (root, args, context) => {
   if (!context.currentUser) {
@@ -10,6 +19,27 @@ const allMessages = async (root, args, context) => {
 
   const messages = await Message.findAll({ order: [['createdAt', 'DESC']] })
   return messages
+}
+
+const sendMessage = (message) => {
+  var transporter = nodemailer.createTransport({
+    host: EMAIL_HOST,
+    port: EMAIL_PORT,
+    secure: true,
+    auth: {
+      user: EMAIL_USERNAME,
+      pass: EMAIL_PASSWORD,
+    },
+  })
+
+  var mailOptions = {
+    from: EMAIL_FROM,
+    to: EMAIL_TO,
+    subject: 'Viesti Tuguriumista',
+    text: message,
+  }
+
+  transporter.sendMail(mailOptions)
 }
 
 const addMessage = async (root, args, context) => {
@@ -22,6 +52,9 @@ const addMessage = async (root, args, context) => {
     important: args.important,
   })
   pubsub.publish('NEW_MESSAGE', { newMessage: message })
+  if (args.important) {
+    sendMessage(args.from + ': ' + args.message)
+  }
   return message
 }
 
--- tugurium-back/util/config.js
+++ tugurium-back/util/config.js
@@ -7,4 +7,10 @@ module.exports = {
   SECRET: process.env.SECRET,
   SENSOR_TOKEN: process.env.SENSOR_TOKEN,
   ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+  EMAIL_USERNAME: process.env.EMAIL_USERNAME,
+  EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+  EMAIL_HOST: process.env.EMAIL_HOST,
+  EMAIL_PORT: process.env.EMAIL_PORT,
+  EMAIL_TO: process.env.EMAIL_TO,
+  EMAIL_FROM: process.env.EMAIL_FROM,
 }
